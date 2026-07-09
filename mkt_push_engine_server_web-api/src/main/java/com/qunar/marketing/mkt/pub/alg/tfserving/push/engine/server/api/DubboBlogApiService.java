6d7a14256020b04808d6ff012cca2bfed1dfe5fb
郭安 <an.guo@qunar.com>
2026-07-09 10:35:10 +0800

    补充完善注释

--- mkt_push_engine_server_web-api/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/api/DubboBlogApiService.java
+++ mkt_push_engine_server_web-api/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/api/DubboBlogApiService.java
@@ -1,9 +1,22 @@
 package com.qunar.marketing.mkt.pub.alg.tfserving.push.engine.server.api;
 
 /**
- * 普通dubbo调用接口
+ * Dubbo同步服务接口
+ * <p>提供Blog查询和模型预测的同步Dubbo调用能力
  */
 public interface DubboBlogApiService {
+
+    /**
+     * 查询Blog（同步）
+     * @param blogId BlogID
+     * @return Blog对象
+     */
     Blog select(Integer blogId);
+
+    /**
+     * 执行模型预测（同步）
+     * @param request 预测请求
+     * @return 预测响应
+     */
     AlgoInnerResponse predict(AlgoInnerRequest request);
 }
