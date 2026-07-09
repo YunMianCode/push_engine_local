ee739be17b9525ff71c21874d30adbfd62ceba65
郭安 <an.guo@qunar.com>
2026-07-09 10:35:05 +0800

    补充完善注释

--- mkt_push_engine_server_web-api/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/api/DubboBetterAsyncService.java
+++ mkt_push_engine_server_web-api/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/api/DubboBetterAsyncService.java
@@ -3,13 +3,23 @@ package com.qunar.marketing.mkt.pub.alg.tfserving.push.engine.server.api;
 import qunar.tc.dubbo.async.Async;
 
 /**
-* @author tcdev
-* @date 2021/9/23 12:00
-* @description 使用qunar封装的dubbo异步，参考wiki：https://wiki.corp.qunar.com/confluence/display/devwiki/Better+Async
-*/
+ * Dubbo异步服务接口
+ * <p>使用Qunar封装的Dubbo异步调用，参考wiki：https://wiki.corp.qunar.com/confluence/display/devwiki/Better+Async
+ */
 @Async
 public interface DubboBetterAsyncService {
+
+    /**
+     * 长任务查询Blog
+     * @param blogId BlogID
+     * @return Blog对象
+     */
     Blog longTaskSelect(Integer blogId);
 
+    /**
+     * 执行模型预测
+     * @param request 预测请求
+     * @return 预测响应
+     */
     AlgoInnerResponse predict(AlgoInnerRequest request);
 }
