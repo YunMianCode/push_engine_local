48de48269762f7e5053be14e4d754d9e3be98355
郭安 <an.guo@qunar.com>
2026-07-09 10:32:29 +0800

    补充完善注释

--- mkt_push_engine_server_web/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/service/utils/constant/CommonConstants.java
+++ mkt_push_engine_server_web/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/service/utils/constant/CommonConstants.java
@@ -1,9 +1,23 @@
+/**
+ * 通用常量类
+ */
 package com.qunar.marketing.mkt.pub.alg.tfserving.push.engine.server.service.utils.constant;
 
 public class CommonConstants {
+
+    /**
+     * 成功消息
+     */
     public static final String SUCCESS_MESSAGE = "成功";
+
+    /**
+     * 内部错误消息
+     */
     public static final String INNER_ERROR_MESSAGE = "内部错误";
 
+    /**
+     * 监控名称
+     */
     public static final String MONITOR_NAME = "modelPredict";
     public static final String MODEL_DIR = "/home/q/www/models";
 }
