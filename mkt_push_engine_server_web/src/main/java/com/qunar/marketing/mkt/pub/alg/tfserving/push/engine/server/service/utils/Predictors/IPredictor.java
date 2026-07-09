4c403fe10adb287c559d020d6ec8e0eb0f6b3af3
郭安 <an.guo@qunar.com>
2026-07-09 10:34:05 +0800

    补充完善注释

--- mkt_push_engine_server_web/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/service/utils/Predictors/IPredictor.java
+++ mkt_push_engine_server_web/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/service/utils/Predictors/IPredictor.java
@@ -4,13 +4,33 @@ import com.qunar.marketing.mkt.pub.alg.tfserving.push.engine.server.service.util
 import com.qunar.marketing.mkt.pub.alg.tfserving.push.engine.server.service.utils.domain.PredictResult;
 import com.qunar.marketing.mkt.pub.alg.tfserving.push.engine.server.service.utils.domain.PredictParam;
 
+/**
+ * 预测器接口
+ * <p>定义模型预测的通用接口，支持TensorFlow、ONNX、PyTorch等多种模型类型
+ */
 public interface IPredictor {
 
+    /**
+     * 执行预测
+     * @param param 预测参数
+     * @return 预测结果
+     */
     public PredictResult predict(PredictParam param);
 
+    /**
+     * 模型预热
+     * <p>提前加载模型到内存，避免首次预测延迟
+     */
     public void warmUp();
 
+    /**
+     * 初始化预测器
+     * @param modelConfig 模型配置
+     */
     public void init(ModelConfig modelConfig);
 
+    /**
+     * 释放资源
+     */
     public void close();
 }
