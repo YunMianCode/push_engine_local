9ee61705d9113e87dffc9fa55378f0282cc6f688
郭安 <an.guo@qunar.com>
2026-07-09 10:28:11 +0800

    补充完善注释

--- mkt_push_engine_server_web/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/controller/WelcomeController.java
+++ mkt_push_engine_server_web/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/controller/WelcomeController.java
@@ -4,8 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+/**
+ * 欢迎页面控制器
+ */
 @Controller
 public class WelcomeController {
+
+    /**
+     * 欢迎页面
+     * @return 欢迎页面视图
+     * @throws Exception 页面渲染异常
+     */
     @GetMapping("/")
     public String welcome() throws Exception {
         return "welcome";
