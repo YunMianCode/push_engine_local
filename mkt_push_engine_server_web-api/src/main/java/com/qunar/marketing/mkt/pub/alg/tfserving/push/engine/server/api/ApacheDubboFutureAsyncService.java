0b63ad7f2e496a82359b4114e837f7a52d3780bc
郭安 <an.guo@qunar.com>
2026-07-09 10:34:58 +0800

    补充完善注释

--- mkt_push_engine_server_web-api/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/api/ApacheDubboFutureAsyncService.java
+++ mkt_push_engine_server_web-api/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/api/ApacheDubboFutureAsyncService.java
@@ -3,11 +3,22 @@ package com.qunar.marketing.mkt.pub.alg.tfserving.push.engine.server.api;
 import java.util.concurrent.CompletableFuture;
 
 /**
-* @author tcdev
-* @date 2021/9/23 12:00
-* @description 使用dubbo官方的CompletableFuture进行异步调用的接口
-*/
+ * Dubbo Future异步服务接口
+ * <p>使用Dubbo官方的CompletableFuture进行异步调用
+ */
 public interface ApacheDubboFutureAsyncService {
+
+    /**
+     * 查询Blog（Future异步）
+     * @param id BlogID
+     * @return CompletableFuture异步结果
+     */
     CompletableFuture<Blog> select(int id);
+
+    /**
+     * 执行模型预测（Future异步）
+     * @param request 预测请求
+     * @return CompletableFuture异步结果
+     */
     CompletableFuture<AlgoInnerResponse> predict(AlgoInnerRequest request);
 }
