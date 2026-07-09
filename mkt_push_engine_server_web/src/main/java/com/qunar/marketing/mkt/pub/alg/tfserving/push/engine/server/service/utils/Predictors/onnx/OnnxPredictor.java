1e41a1e7f41d703bb1faf69ff1af9d3871494721
郭安 <an.guo@qunar.com>
2026-07-09 10:33:45 +0800

    补充完善注释

--- mkt_push_engine_server_web/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/service/utils/Predictors/onnx/OnnxPredictor.java
+++ mkt_push_engine_server_web/src/main/java/com/qunar/marketing/mkt/pub/alg/tfserving/push/engine/server/service/utils/Predictors/onnx/OnnxPredictor.java
@@ -5,23 +5,42 @@ import com.qunar.marketing.mkt.pub.alg.tfserving.push.engine.server.service.util
 import com.qunar.marketing.mkt.pub.alg.tfserving.push.engine.server.service.utils.domain.PredictParam;
 import com.qunar.marketing.mkt.pub.alg.tfserving.push.engine.server.service.utils.domain.PredictResult;
 
+/**
+ * ONNX预测器
+ * <p>ONNX模型的预测器实现（预留）
+ */
 public class OnnxPredictor implements IPredictor {
 
+    /**
+     * 执行ONNX模型预测
+     * @param predictParam 预测参数
+     * @return 预测结果
+     */
     @Override
     public PredictResult predict(PredictParam predictParam) {
         return null;
     }
 
+    /**
+     * 初始化ONNX预测器
+     * @param modelConfig 模型配置
+     */
     @Override
     public void init(ModelConfig modelConfig) {
 
     }
 
+    /**
+     * 释放ONNX资源
+     */
     @Override
     public void close() {
 
     }
 
+    /**
+     * ONNX模型预热
+     */
     @Override
     public void warmUp()
     {
